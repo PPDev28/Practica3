7aaaf3f70d7b8b567417fa7ebdf88c0fd32f01da
jose.ortegap <jose.ortegap@alten.es>
2023-05-11 14:33:37 +0200

    fix [CT]/navigation-launcher-screen

--- app/src/main/java/com/example/practica3/fragments/LauncherFragment.kt
+++ app/src/main/java/com/example/practica3/fragments/LauncherFragment.kt
@@ -1,7 +1,23 @@
 package com.example.practica3.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.practica3.R
+import com.example.practica3.databinding.FragmentLauncherBinding
 
 
-class LauncherFragment : Fragment(R.layout.fragment_launcher)
+class LauncherFragment : Fragment(R.layout.fragment_launcher){
+
+    private val binding by lazy { FragmentLauncherBinding.inflate(layoutInflater)}
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return binding.root
+    }
+}
