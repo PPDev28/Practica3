894287f2623be5c068b8f661dafea874a36f0150
jose.ortegap <jose.ortegap@alten.es>
2023-05-13 14:50:44 +0200

    (2) fix[CS]/navigation-launcher-screen

--- app/src/main/java/com/example/practica3/NavigationActivity.kt
+++ app/src/main/java/com/example/practica3/NavigationActivity.kt
@@ -6,10 +6,9 @@ import com.example.practica3.databinding.NavigationMainBinding
 
 class NavigationActivity : AppCompatActivity() {
 
-    private val binding by lazy { NavigationMainBinding.inflate(layoutInflater)}
+    private val binding by lazy { NavigationMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
     }
 }
